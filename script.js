3d82c5d9243d02d5e54666ff42ace7b8e9777ac1
pseudominimalista <dfern3321@gmail.com>
2023-05-06 14:25:01 -0300

    MAIS ANIMAÇÕES

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ document.addEventListener('DOMContentLoaded', () => {
     html.setAttribute('data-theme', isLight? 'light':'dark')
     switchTheme.innerHTML = isLight? sun : moon
 })
+
+
+
+// https://www.youtube.com/watch?v=T33NN_pPeNI&t=49
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+        entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
