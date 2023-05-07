e4c5bdd183929668a0a79ef1dbba0b7b4aa20a9d
John Doe <120287606+pseudominimalista@users.noreply.github.com>
2023-05-06 22:45:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -102,9 +102,3 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
-
-if ('serviceWorker' in navigator) {
-
-    navigator.serviceWorker.register('/sw.js')
-
-}
